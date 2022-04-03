93ff5f46ee64aafd5e6010ef7e521286e351066e
Lenny Atomz <lenny.atomz@gmail.com>
2022-04-02 22:07:31 -0400

    doc: add code comments

--- algorithms/binarysearch/BinarySearch.java
+++ algorithms/binarysearch/BinarySearch.java
@@ -13,20 +13,26 @@ public class BinarySearch
     {
         if(numbers.length == 0) return -1;
 
+        // Q: Why is right 1 less than the length of numbers array?
+        // A: Because left is 0-indexed; while loop would throw an exception if full length was used.
         int left = 0;
         int right = numbers.length - 1;
 
         while(left <= right)
         {
+            // Iteration 0: midpoint = (0 + (8-0)) / 2 = 8/2 = 4
             int midpoint = left + (right - left) / 2;
-            if (numbers[midpoint] == target)
+            // In this case, the target value has been found.
+            if (numbers[midpoint] == target) 
             {
                 return midpoint;
             }
+            // In this case, the target value is smaller, so search the left half.
             else if(numbers[midpoint] > target)
             {
                 right = midpoint - 1;
             }
+            // In this case, the target value is larger, so search the right half.
             else 
             {
                 left = midpoint + 1;
@@ -35,10 +41,14 @@ public class BinarySearch
         return -1;
     }
 
+    /**
+     * The main() method is where the program begins.
+     */
     public static void main(String[] args)
     {
         int array[] = {11,22,33,44,55,66,77,88,99};
         int searchTerm = 88;
+        // This is where binarySearch() is first invoked.
         if(binarySearch(array, searchTerm) == -1)
         {
             System.out.println("Not Found");
