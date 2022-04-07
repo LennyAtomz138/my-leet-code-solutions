8917ae2a25e5ec2e632a3791298694c0132222af
Lenny Atomz <lenny.atomz@gmail.com>
2022-04-07 18:27:59 -0400

    chore: update code prior to hashmap implementation

--- algorithms/romantointeger/RomanToInteger.java
+++ algorithms/romantointeger/RomanToInteger.java
@@ -37,37 +37,9 @@ public class RomanToInteger
 
         int accumulator = 0;
 
-        /** TODO: Fix case where s = MCMXCIV; should be 1994, not 2216; 2216-1994=222
-         * Note that amounts need to be subtracted for the following scenarios:
-         * CM = 900
-         * XC = 90
-         * IV = 4
-         *
-         * ==For reference==:
-         * (https://www.romannumerals.org/blog/rules-for-formation-of-roman-numerals-15#:~:text=In%20any%20given%20Roman%20number%2C%20if%20a%20symbol,than%20one%20value%20%28symbol%29%20from%20a%20Roman%20numeral.)
-         * Rule 4: When Should I Subtract?
-         * In any given Roman number, if a symbol is placed before a larger one, you subtract.
-         * In other words, when you see a smaller number is on the left of a larger one, you subtract.
-         *
-         * A note to remember here is that while you can add more than two values,
-         * you cannot subtract more than one value (symbol) from a Roman numeral.
-         *
-         * Example:
-         *
-         * IV = V − I = 5 − 1 = 4
-         *
-         * IX = X − I = 10 − 1 = 9
-         *
-         * XL = L - X = 50 - 10 = 40
-         *
-         * XC = C - X = 100 - 10 = 90
-         *
-         * TODO: Can I still go with a switch statement here? Need to check for Rule #4!!!
-         */
-        // Iterate over `char[]` array using enhanced for-loop.
+        // Iterate over `char[]` array and aggregate respective values.
         for(int i = 0; i < chars.length; i++)
         {
-            // change it to a number
             switch (chars[i])
             {
                 case 'I':
@@ -95,6 +67,40 @@ public class RomanToInteger
                     break;
             }
         }
+
+        /** TODO: Fix case where s = MCMXCIV; should be 1994, not 2216; 2216-1994=222
+         * Note that amounts need to be subtracted for the following scenarios:
+         * CM = 900
+         * XC = 90
+         * IV = 4
+         *
+         * ==For reference==:
+         * (https://www.romannumerals.org/blog/rules-for-formation-of-roman-numerals-15#:~:text=In%20any%20given%20Roman%20number%2C%20if%20a%20symbol,than%20one%20value%20%28symbol%29%20from%20a%20Roman%20numeral.)
+         * Rule 4: When Should I Subtract?
+         * In any given Roman number, if a symbol is placed before a larger one, you subtract.
+         * In other words, when you see a smaller number is on the left of a larger one, you subtract.
+         *
+         * A note to remember here is that while you can add more than two values,
+         * you cannot subtract more than one value (symbol) from a Roman numeral.
+         *
+         * Example:
+         *
+         * IV = V − I = 5 − 1 = 4
+         *
+         * IX = X − I = 10 − 1 = 9
+         *
+         * XL = L - X = 50 - 10 = 40
+         *
+         * XC = C - X = 100 - 10 = 90
+         *
+         * TODO: Can I still go with a switch statement here? Need to check for Rule #4!!!
+         * Brainstorm...
+         * * Maybe use compare() to see if left is less than right
+         * * Consider parsing string once to see if any invoke Rule #4
+         */
+        // Parse char[] and subtract values based on cases.
+
+
         return accumulator;
     }
 
